d88b208a05f0567a67b1d491130789ad0158bb20
Sabrina Ren <sren22@uwo.ca>
2016-03-01 23:20:02 -0500

    Sliders for position, scale, and transparency of head and ventricles with title

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -1,56 +1,91 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ButtonBehaviour : MonoBehaviour {
-    public GameObject[] ventricles;
+    GameObject[] ventricles;
+	int activeVentricleIndex;
+
+	[SerializeField] private GameObject mannequin;
+	[SerializeField] private GameObject head;
+	[SerializeField] private Text _nameText;
+	[SerializeField] private GameObject _menuPanel;
 
     void Start()
     {
+		// Hide all ventricles except one
         ventricles = GameObject.FindGameObjectsWithTag("Ventricle");
-        for (int i = 0; i < ventricles.Length - 1; i++)
+        for (int i = 1; i < ventricles.Length; i++)
         {
             Debug.Log("come in here");
             ventricles[i].SetActive(false);
         }
+		activeVentricleIndex = 0;
+
+		// Show ventricle name
+		_nameText.text = ventricles [activeVentricleIndex].transform.parent.name;
+
+		_menuPanel.SetActive (false);
     }
+
+	public void MenuClick()
+	{
+		// Toggle menu display
+		_menuPanel.SetActive (!_menuPanel.activeInHierarchy);
+	}
+
 
     public void RightClick()
     {
-        for (int i = 0; i < ventricles.Length; i++)
-        {
-            if(ventricles[i].activeSelf == true)
-            {
-                ventricles[i].SetActive(false);
-                if (i == ventricles.Length-1)
-                {
-                    ventricles[0].SetActive(true);
-                }
-                else
-                {
-                    ventricles[i+1].SetActive(true);
-                }
-                break;
-            }
-        }
+		ventricles [activeVentricleIndex].SetActive (false);
+		activeVentricleIndex = (activeVentricleIndex + 1) % ventricles.Length;
+		ventricles [activeVentricleIndex].SetActive (true);
+		_nameText.text = ventricles [activeVentricleIndex].transform.parent.name;
     }
 
     public void LeftClick()
     {
-        for (int i = 0; i < ventricles.Length; i++)
-        {
-            if (ventricles[i].activeSelf == true)
-            {
-                ventricles[i].SetActive(false);
-                if (i == 0)
-                {
-                    ventricles[ventricles.Length - 1].SetActive(true);
-                }
-                else
-                {
-                    ventricles[i - 1].SetActive(true);
-                }
-                break;
-            }
-        }
+
+		ventricles [activeVentricleIndex].SetActive (false);
+		activeVentricleIndex = (activeVentricleIndex - 1 + ventricles.Length) % ventricles.Length;
+		ventricles [activeVentricleIndex].SetActive (true);
+		_nameText.text = ventricles [activeVentricleIndex].transform.parent.name;
     }
+
+	public void PositionXChanged(float value) {
+		Vector3 position = mannequin.transform.localPosition;
+		position.x = value;
+		mannequin.transform.localPosition = position;
+	}
+
+	public void PositionYChanged(float value) {
+		Vector3 position = mannequin.transform.localPosition;
+		position.y = value;
+		mannequin.transform.localPosition = position;
+	}
+
+	public void PositionZChanged(float value) {
+		Vector3 position = mannequin.transform.localPosition;
+		position.z = value;
+		mannequin.transform.localPosition = position;
+	}
+
+	public void TransparencyVentriclesChanged(float value) {
+		Renderer[] renderers = ventricles [activeVentricleIndex].GetComponentsInChildren<Renderer> ();
+		for (int i = 0; i < renderers.Length; i++) {
+			Color color = renderers [i].material.color;
+			color.a = value;
+			renderers [i].material.color = color;
+		}
+	}
+
+	public void TransparencyHeadChanged(float value) {
+		Color color = head.GetComponent<Renderer> ().material.color;
+		color.a = value;
+		head.GetComponent<Renderer> ().material.color = color;
+	}
+
+	public void ScaleValueChanged(float value) {
+		mannequin.transform.localScale = new Vector3 (value, value, value);
+	}
 }
